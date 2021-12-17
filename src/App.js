5a5f4dcf340b4f97a011cd1fd1e15e11f8c5d5ae
Carter <carter.day96@outlook.com>
2021-12-16 19:26:57 -0500

    add uuid pckg rating select and addFeedback func

--- src/App.js
+++ src/App.js
@@ -6,27 +6,31 @@ import FeedbackData from "./data/FeedbackData"
 import FeedbackForm from "./components/FeedbackForm"
 
 function App() {
-    
-    const [feedback, setFeedback] = useState (FeedbackData)
+	const [feedback, setFeedback] = useState(FeedbackData)
 
-    const deleteFeedback = (id) => {
-        if (window.confirm("Confirm Delete?")) {
-            setFeedback(feedback.filter(
-                (item) => item.id !== id
-            ))
-        }
-    }
+	const deleteFeedback = (id) => {
+		if (window.confirm("Confirm Delete?")) {
+			setFeedback(feedback.filter((item) => item.id !== id))
+		}
+	}
 
-    return (
-        <>
-            <Header />
-            <div className = "container">
-                <FeedbackForm />
-                <FeedbackStats feedback = { feedback } />
-                <FeedbackList feedback = { feedback } handleDelete = { deleteFeedback } />
-            </div>
-        </>
-    )
+	const addFeedback = (newFeedback) => {
+		console.log(newFeedback)
+	}
+
+	return (
+		<>
+			<Header />
+			<div className="container">
+				<FeedbackForm handleAdd={addFeedback} />
+				<FeedbackStats feedback={feedback} />
+				<FeedbackList
+					feedback={feedback}
+					handleDelete={deleteFeedback}
+				/>
+			</div>
+		</>
+	)
 }
 
 export default App
